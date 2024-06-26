2f6715182d87d2ac80005c916f5186b7653a3f99
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2024-06-26 12:51:46 +0200

    initial version with 2D-3D conversion
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- pubgrep.py
+++ pubgrep.py
@@ -1,11 +1,13 @@
 from urllib.parse import quote
 import argparse
 from pathlib import Path
+import subprocess as sp
 
 import requests
 
 
 COMPOUND_DIR_NAME = "pubchem_compounds"
+HLGAP_THR = 0.5
 
 
 def rawurlencode(string):
@@ -53,6 +55,53 @@ def test_pubchem_server():
         )
 
 
+# Function to run the xtb command
+def run_xtb_command(xtb_path: Path, calc_dir: Path, args: list) -> tuple | None:
+    """
+    This function is used to run the xtb command.
+    """
+    try:
+        xtb_out = sp.run(
+            [xtb_path, *args],
+            check=True,
+            stdout=sp.PIPE,
+            stderr=sp.PIPE,
+            cwd=calc_dir,
+        )
+        stdout = xtb_out.stdout.decode()
+        stderr = xtb_out.stderr.decode()
+
+        return stdout, stderr
+
+    except sp.CalledProcessError as e:
+        with open(calc_dir / "xtb.out", "w", encoding="utf-8") as file:
+            file.write(e.stdout.decode())
+        with open(calc_dir / "xtb.err", "w", encoding="utf-8") as file:
+            print(
+                f"Command '{e.cmd}' returned non-zero exit status {e.returncode}.",
+                file=file,
+            )
+            file.write(e.stderr.decode())
+        raise e
+
+
+def check_hlgap(output: str) -> bool:
+    """
+    This function is used to check if the HOMO-LUMO gap of an xtb output is large enough.
+    """
+    hlgap = None
+    for line in output.split("\n"):
+        if "HOMO-LUMO GAP" in line:
+            hlgap = float(line.split()[3])
+    if not hlgap:
+        raise ValueError("HOMO-LUMO gap not determined.")
+
+    if hlgap >= HLGAP_THR:
+        return True
+    else:
+        return False
+
+
 def search_compound(compound, input_format):
     """
     This function is used to search the compound in the PubChem database.
@@ -84,6 +133,7 @@ def main():
     output_format = args.output
     skip = args.skip
 
+    xtb_path = "xtb"
     if not skip:
         test_pubchem_server()
 
@@ -105,9 +155,6 @@ def main():
         else:
             found_compounds.append((compound, result))
 
-    print(f"Found: {found_compounds}")
-    print(f"Not Found: {not_found_compounds}")
-
     if output_format == "sdf":
         for cid, compound in found_compounds:
             cid_dir = Path(f"{COMPOUND_DIR_NAME}/{cid}")
@@ -116,8 +163,55 @@ def main():
                 f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{cid}/sdf?record_type=3d",
                 timeout=10,
             )
-            with open(f"{cid_dir}/{compound}.sdf", "w", encoding="utf-8") as file:
-                file.write(response.text)
+            if "PUGREST.NotFound" in response.text:
+                print(
+                    f"No 3D Conformer Data found for CID {cid}. Retrieving 2D Conformer Data instead."
+                )
+                response = requests.get(
+                    f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{cid}/sdf",
+                    timeout=10,
+                )
+                with open(f"{cid_dir}/{cid}_2d.sdf", "w", encoding="utf-8") as file:
+                    file.write(response.text)
+
+                # Convert 2D to 3D
+                try:
+                    xtb_args = [
+                        f"{cid}_2d.sdf",
+                        "--gfn",
+                        "2",
+                        "--sp",
+                        "--ceasefiles",
+                    ]
+                    xtb_out, xtb_err = run_xtb_command(
+                        xtb_path=xtb_path, calc_dir=cid_dir, args=xtb_args
+                    )
+                    if "converted geometry written to" in xtb_out:
+                        print("3D conversion successful.")
+                        Path(f"{cid_dir}/gfnff_convert.sdf").rename(
+                            f"{cid_dir}/{cid}.sdf"
+                        )
+                    else:
+                        raise Exception("3D conversion failed.")
+                    if not check_hlgap(xtb_out):
+                        raise ValueError("HOMO-LUMO gap too small!")
+                except Exception as e:
+                    print("xtb structure conversion failed. Error code:")
+                    print(e)
+
+                files_to_delete = [
+                    ".sccnotconverged",
+                    "convert.log",
+                    "mdrestart",
+                    "xtb.trj",
+                    "xtbmdok",
+                ]
+                for file in files_to_delete:
+                    if Path(f"{cid_dir}/{file}").exists():
+                        Path(f"{cid_dir}/{file}").unlink()
+            else:
+                with open(f"{cid_dir}/{cid}.sdf", "w", encoding="utf-8") as file:
+                    file.write(response.text)
     elif output_format in ["logp", "logP"]:
         with open("pubchem_logP.data", "w", encoding="utf-8") as file:
             for compound, cid in found_compounds:
