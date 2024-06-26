10c049d4e5be7ef8babd55f054ccc2a485252f9e
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2024-06-26 22:45:31 +0200

    better version handling
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- pubgrep/pubgrep.py
+++ pubgrep/pubgrep.py
@@ -7,7 +7,7 @@ import shutil
 import requests
 
 try:
-    from .pubgrep_version import version as __version__ # type: ignore
+    from ._version import version as __version__ # type: ignore
 except ImportError:
     __version__ = "0.0.0"
 
